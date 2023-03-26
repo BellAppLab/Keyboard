44ed67aa8fa2dec0998e7e7a920b006fb9ad2872
André Abou Chami Campana <andreabou@me.com>
2023-03-26 16:22:44 -0300

    Updating package

--- Package.swift
+++ Package.swift
@@ -4,14 +4,26 @@ import PackageDescription
 
 let package = Package(
     name: "Keyboard",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_13),
+        .tvOS(.v13),
+        .watchOS(.v5)
+    ],
     products: [
-        .library(name: "Keyboard",
-                 targets: ["Keyboard"]),
-        ],
+        .library(
+            name: "Keyboard",
+            type: .static,
+            targets: ["iOS Example"]),
+    ],
     targets: [
         .target(
             name: "Keyboard"
         ),
+        .testTarget(
+            name: "iOS Tests",
+            dependencies: ["Keyboard"]
+        ),
     ],
-    swiftLanguageVersions: [4.2, 5.0]
+    swiftLanguageVersions: [.v5]
 )
